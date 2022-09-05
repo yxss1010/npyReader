b01f25171a7b480e643577792b49af7e1bbf7466
yxss1010 <84307666+yxss1010@users.noreply.github.com>
2022-09-05 23:25:47 +0800

    Update Demo.java

--- src/Demo.java
+++ src/Demo.java
@@ -10,56 +10,13 @@ public class Demo {
     public static void main(String[] args) throws Exception {
         String path = "./x.npy";
         DataInputStream dataInputStream = new DataInputStream(new FileInputStream(path));
-        int[][] arr = readIntArray(dataInputStream);
-
+        int[][] arr = NumpyReader.readIntArray(dataInputStream);
+        for(int i[] : arr){
+            for(int j : i){
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
         System.out.println();
-    }
-
-    /**
-     * 读取整数型数组
-     * @param inputStream
-     * @return
-     * @throws Exception
-     */
-    public static int[][] readIntArray(InputStream inputStream) throws Exception {
-        String version = NumpyReader.readVersion(inputStream);
-        String header = NumpyReader.readArrayHeader(inputStream, version);
-        int itemLength = NumpyReader.itemLength(header);
-        String shape = NumpyReader.arrayShape(header);
-        int row = Integer.valueOf(shape.split(",")[0]);
-        int col = Integer.valueOf(shape.split(",")[1]);
-        int[][] array = new int[row][col];
-        byte[] item = new byte[itemLength];
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
-                inputStream.read(item);
-                array[i][j] = NumpyReader.bytes2Int(item);
-            }
-        }
-        return array;
-    }
-
-    /**
-     * 读取double数组
-     * @param inputStream
-     * @return
-     * @throws Exception
-     */
-    public static double[][] readDoubleArray(InputStream inputStream) throws Exception {
-        String version = NumpyReader.readVersion(inputStream);
-        String header = NumpyReader.readArrayHeader(inputStream, version);
-        int itemLength = NumpyReader.itemLength(header);
-        String shape = NumpyReader.arrayShape(header);
-        int row = Integer.valueOf(shape.split(",")[0]);
-        int col = Integer.valueOf(shape.split(",")[1]);
-        double[][] array = new double[row][col];
-        byte[] item = new byte[itemLength];
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
-                inputStream.read(item);
-                array[i][j] = NumpyReader.bytes2Double(item);
-            }
-        }
-        return array;
     }
 }
